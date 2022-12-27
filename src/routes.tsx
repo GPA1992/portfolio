7eb1c68962723c97456ea92673aab39fe79ecb71
Gabriel <gabrielproencaaraujo@gmail.com>
2022-12-27 15:48:09 -0300

    Hash Router Test

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Main from './views/main';
 import GlobalContextProvider from './contexts/GlobalContextProvider';
 
 export default function MainRoutes() {
 	return (
 		<GlobalContextProvider>
-			<Routes>
+			<HashRouter>
+				{/* <Routes> */}
 				<Route path="/portfolio" element={<h1>Portfólio</h1>} />
 				<Route path="/portfolio/main" element={<Main />} />
 				<Route path="*" element={<h1> 404 - Not found </h1>} />
-			</Routes>
+				{/* </Routes> */}
+			</HashRouter>
 		</GlobalContextProvider>
 	);
 }
