a4c3ce642d8a7ea072ff4bfdaf4e7ba69439d8fe
Gabriel <gabrielproencaaraujo@gmail.com>
2022-12-27 15:41:41 -0300

    Routes Fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from './views/main';
 import GlobalContextProvider from './contexts/GlobalContextProvider';
-import useUser from './hooks/useUser';
-
-type Props = {
-	redirectTo: string;
-};
-
-function ProtectedRoutes({ redirectTo }: Props) {
-	const { token } = useUser();
-
-	return token ? <Outlet /> : <Navigate to={redirectTo} />;
-}
 
 export default function MainRoutes() {
 	return (
 		<GlobalContextProvider>
 			<Routes>
-				<Route path="/" element={<h1>Portfólio</h1>} />
-				<Route element={<ProtectedRoutes redirectTo="/" />}>
-					<Route path="/main" element={<Main />} />
-				</Route>
+				<Route path="/portfolio" element={<h1>Portfólio</h1>} />
+				<Route path="/portfolio/main" element={<Main />} />
 				<Route path="*" element={<h1> 404 - Not found </h1>} />
 			</Routes>
 		</GlobalContextProvider>
